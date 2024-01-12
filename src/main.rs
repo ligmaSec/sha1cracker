0a708599d45db7d1a42499cfb2aa7d8715bade7d
ligmaSec <you@example.com>
2024-01-12 11:57:49 +0100

    many broken things but fk it we ball

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, process, str};
-use std::fs::read_to_string;
+use std::fs::File;
 use sha1_smol::Sha1;
+use std::io::Read;
 
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
@@ -9,12 +10,13 @@ fn print_usage() {
        println!("{}",String::from("Usage: ./sha1cracker [WORDLIST-FILE] [SHA1-HASH]"));
 }
 
-fn read_lines(filename: &str)-> Vec<String> {
-    read_to_string(filename)
-        .unwrap() //panic on file read error
-        .lines() // returns iterator
-        .map(String::from) //convert each line to a String
-        .collect() // satisfy vector return
+fn read_lines(mut filename: String)-> Vec<u8> {
+    let mut file_content;
+    let mut file = Read::read_to_end(&mut file_content).expect("unable to open file");
+
+    file.read_to_end(file_content);
+    file_content
+    
 }
 
 
@@ -28,16 +30,22 @@ fn main() {
 
     let wordlist = args[1].trim();
     let sha1_hash = args[2].trim();
-    //if sha1_hash.len() != SHA1_HEX_STRING_LENGTH {
-    //    println!("invalid hash length, must be 40 characters");
-    //}
+    if sha1_hash.len() != SHA1_HEX_STRING_LENGTH {
+        println!("invalid hash length, must be 40 characters");
+    }
     
     //let mut word: &[u8] = "hello".as_bytes();
 
     let mut hasher = Sha1::new();
     //hasher.update(word);
-    for line in read_lines(wordlist){
-       println!("{:?}",line.as_bytes()); 
+    for line in read_lines(wordlist).lines(){
+       hasher.update(line.as_bytes());
+       println!("{}", hasher.digest().to_string());
+       if hasher.digest().to_string() == sha1_hash.to_string() {
+            println!("the clear text hash is {}",line);
+            process::exit(0);
+       }
+       hasher.reset();
     }
     
 
